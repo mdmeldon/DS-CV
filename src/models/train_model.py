b4b5bed651236479c3a33bafbb1a1334ccb81004
mdmeldon <mdmeldon@gmail.com>
2020-06-05 18:31:49 +0300

    пайплайн обучения

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -2,12 +2,14 @@
 from sklearn.metrics import confusion_matrix, accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
 
 #import torchvision
 # from models.resnet import resnet50
-from auc import AUCCallback
+from .auc import AUCCallback
 import torchvision
 import torch
+import torch.nn as nn
 from torch.utils.data import DataLoader, TensorDataset
 from catalyst.dl import SupervisedRunner
 from catalyst.dl.utils import set_global_seed, prepare_cudnn
@@ -30,9 +32,31 @@ import sys
 import IPython.display as ipd  # To play sound in the notebook
 import warnings
 from pathlib import Path
+from ..dirs import DIR_DATA_PROCESSED
 # ignore warnings 
 if not sys.warnoptions:
     warnings.simplefilter("ignore")
+
+def fit_X_scaler(X_train):
+    """fit StandardScaler，and return StandardScaler object
+    """
+    sc = StandardScaler()
+    for _, clips in enumerate(X_train):
+        data_i_truncated = np.squeeze(clips)
+        sc.partial_fit(data_i_truncated)
+    return sc
+
+def get_X_scaled(X_train, scaler=None):
+    """apply normlization
+    """
+    X_train_new = np.zeros(X_train.shape)
+    for indx, clips in enumerate(X_train):
+        data_i_truncated = np.squeeze(clips)
+        if scaler is not None:  # normlize
+            data_i_truncated = scaler.transform(data_i_truncated)
+        X_train_new[indx, 0, :, :] = data_i_truncated
+    return X_train_new
+
 
 
 if __name__ == '__main__':
@@ -42,8 +66,8 @@ if __name__ == '__main__':
 
     
     DIR_DATA = Path(__file__).absolute().parent.parent / 'data'
-    ref = pd.read_csv("data/processed/Data_path.csv")
-    mfcc = np.load('data/processed/MFCC_PREPARE_AUG.npy')
+    ref = pd.read_csv(str(DIR_DATA_PROCESSED / 'Data_path.csv'))
+    mfcc = np.load(str(DIR_DATA_PROCESSED / 'MFCC_PREPARE_AUG.npy'))
 
     # Split between train and test 
     X_train, X_test, y_train, y_test = train_test_split(mfcc,
@@ -60,29 +84,27 @@ if __name__ == '__main__':
     y_test = lb.fit_transform(y_test) # to_categorical
 
     # Normalization as per the standard NN process
-    mean = np.mean(X_train, axis=0)
-    std = np.std(X_train, axis=0)
-
-    X_train = (X_train - mean)/std
-    X_test = (X_test - mean)/std
+    sc = fit_X_scaler(X_train)
+    X_train = get_X_scaled(X_train, sc)
+    X_test = get_X_scaled(X_test, sc)
 
     classes_names = list(lb.classes_)
 
     model = torchvision.models.resnet50(pretrained=True,progress=True)
 
     # Изменение количества выходных классов 
+    first_conv_layer = model.conv1
+    model.conv1= nn.Sequential(
+                            nn.Conv2d(1, 3, kernel_size=3, stride=1, padding=1, dilation=1, groups=1, bias=True),
+                            first_conv_layer
+    )  
+
+    n_class = 12
     num_ftrs = model.fc.in_features
     model.fc = torch.nn.Sequential(
                                 torch.nn.Dropout(0.2),
-                                torch.nn.Linear(num_ftrs, N_CLASS)
-    )
-    model.maxpool = torch.nn.Sequential(
-                                        torch.nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
-                                        torch.nn.Dropout(0.2)
-
-    # Изменение количества входных слоёв
-    model.conv1 = torch.nn.Conv2d(30, 64, kernel_size=7, stride=2, padding=3,
-                                bias=False)    
+                                torch.nn.Linear(num_ftrs, n_class)
+    )   
 
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
@@ -107,7 +129,7 @@ if __name__ == '__main__':
     # Init catalyst components
     runner = SupervisedRunner(device=device)
     loaders = {'train': train_loader, 'valid': valid_loader}
-    logdir = 'data/logs/0'
+    logdir = 'data/logs/2'
     callbacks = [
                 AUCCallback(num_classes = 12, class_names= classes_names),
                 AccuracyCallback(),
@@ -120,7 +142,7 @@ if __name__ == '__main__':
         optimizer=optimizer,
         loaders=loaders,
         callbacks=callbacks,
-        logdir=logdir,
+        # logdir=logdir,
         num_epochs=120,
         verbose=True,        
     )  
